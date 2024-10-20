7a7f5ff71073d1acb28cde274fb926c7a33a9075
muk3shh07 <dhamimukesh076@gmail.com>
2024-10-20 22:24:56 +0545

    comment added

--- EmailValidate/main.py
+++ EmailValidate/main.py
@@ -2,6 +2,7 @@ import re
 email_condition = "^[a-z]+[\\._]?[a-z 0-9] + [@]\w{2,3}$"
 user_email = input("Enter your Email::")
 
+#ifconditions
 if re.search(email_condition,user_email):
     print("Proceeeed")
 else:
